5d031ce7812eb04dda3860e2f0af18ef29152559
Emily <emilyruthgentry@gmail.com>
2019-05-04 10:28:50 -0700

    Creating a random maze

---
+++ Assets/Scripts/MazeCellEdge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCellEdge : MonoBehaviour {
+    public MazeCell cell, otherCell;
+    public MazeDirection direction;
+
+    public void Initialize(MazeCell cell, MazeCell otherCell, MazeDirection direction)
+    {
+        this.cell = cell;
+        this.otherCell = otherCell;
+        this.direction = direction;
+        cell.SetEdge(direction, this);
+        transform.parent = cell.transform;
+        transform.localPosition = Vector3.zero;
+    }
+
+    private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+
+    public MazeCellEdge GetEdge(MazeDirection direction) {
+        return edges[(int)direction];
+    }
+
+    public void SetEdge(MazeDirection direction, MazeCellEdge edge) {
+        edges[(int)direction] = edge;
+    }
+}
